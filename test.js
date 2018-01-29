097a51411f9b17aa93229bc7532ee54dfbfb6a24
MITSUNARI Shigeo <herumi@nifty.com>
2018-01-30 05:33:25 +0900

    increse loop count for bench

--- test.js
+++ test.js
@@ -295,8 +295,7 @@ function bench (label, count, func) {
   console.log(label + ' ' + t)
 }
 
-function benchPairing () {
-  console.log('class')
+function benchAll () {
   const a = new mcl.Fr()
 
   const msg = 'hello wasm'
@@ -311,31 +310,32 @@ function benchPairing () {
 
   console.log('benchmark')
   const C = 100
+  const C2 = 1000
   bench('T_Fr::setByCSPRNG', C, () => a.setByCSPRNG())
   bench('T_pairing', C, () => mcl.pairing(P, Q))
   bench('T_millerLoop', C, () => mcl.millerLoop(P, Q))
   bench('T_finalExp', C, () => mcl.finalExp(e))
   bench('T_precomputedMillerLoop', C, () => mcl.precomputedMillerLoop(P, Qcoeff))
-  bench('T_G1::add', C, () => { P = mcl.add(P, P2) })
-  bench('T_G1::dbl', C, () => { P = mcl.dbl(P) })
+  bench('T_G1::add', C2, () => { P = mcl.add(P, P2) })
+  bench('T_G1::dbl', C2, () => { P = mcl.dbl(P) })
   bench('T_G1::mul', C, () => { P = mcl.mul(P, a) })
-  bench('T_G2::add', C, () => { Q = mcl.add(Q, Q2) })
-  bench('T_G2::dbl', C, () => { Q = mcl.dbl(Q) })
+  bench('T_G2::add', C2, () => { Q = mcl.add(Q, Q2) })
+  bench('T_G2::dbl', C2, () => { Q = mcl.dbl(Q) })
   bench('T_G2::mul', C, () => { Q = mcl.mul(Q, a) })
   bench('T_hashAndMapToG1', C, () => mcl.hashAndMapToG1(msg))
   bench('T_hashAndMapToG2', C, () => mcl.hashAndMapToG2(msg))
 
   let b = new mcl.Fr()
   b.setByCSPRNG()
-  bench('T_Fr::add', C, () => { b = mcl.add(b, a) })
-  bench('T_Fr::mul', C, () => { b = mcl.mul(b, a) })
-  bench('T_Fr::sqr', C, () => { b = mcl.sqr(b) })
-  bench('T_Fr::inv', C, () => { b = mcl.inv(b) })
+  bench('T_Fr::add', C2, () => { b = mcl.add(b, a) })
+  bench('T_Fr::mul', C2, () => { b = mcl.mul(b, a) })
+  bench('T_Fr::sqr', C2, () => { b = mcl.sqr(b) })
+  bench('T_Fr::inv', C2, () => { b = mcl.inv(b) })
 
   let e2 = mcl.pairing(P, Q)
-  bench('T_GT::add', C, () => { e2 = mcl.add(e2, e) })
-  bench('T_GT::mul', C, () => { e2 = mcl.mul(e2, e) })
-  bench('T_GT::sqr', C, () => { e2 = mcl.sqr(e2) })
+  bench('T_GT::add', C2, () => { e2 = mcl.add(e2, e) })
+  bench('T_GT::mul', C2, () => { e2 = mcl.mul(e2, e) })
+  bench('T_GT::sqr', C2, () => { e2 = mcl.sqr(e2) })
   bench('T_GT::inv', C, () => { e2 = mcl.inv(e2) })
 
   Qcoeff.destroy()
@@ -366,7 +366,3 @@ function benchPairingCapi () {
 }
 */
 
-function benchAll () {
-  benchPairing()
-//  benchPairingCapi()
-}
