775878de27724c4965437be465f89aa174fa4f1a
MITSUNARI Shigeo <herumi@nifty.com>
2021-02-01 11:35:01 +0900

    tweat bench

--- test/bench.js
+++ test/bench.js
@@ -495,8 +495,8 @@ function bench (label, count, func) {
   }
   const end = performance.now()
   const t = (end - start) / count
-  const roundTime = (Math.round(t * 1000)) / 1000
-  console.log(label + ' ' + roundTime)
+  const roundTime = (Math.round(t * 1e6)) / 1000
+  console.log(label + ' ' + roundTime + ' usec')
 }
 
 function benchAll () {
@@ -515,42 +515,59 @@ function benchAll () {
   console.log('benchmark')
   const C = 100
   const C2 = 10000
-  bench('T_Fr::setByCSPRNG', C, () => a.setByCSPRNG())
-  bench('T_pairing', C, () => mcl.pairing(P, Q))
-  bench('T_millerLoop', C, () => mcl.millerLoop(P, Q))
-  bench('T_finalExp', C, () => mcl.finalExp(e))
-  bench('T_precomputedMillerLoop', C, () => mcl.precomputedMillerLoop(P, Qcoeff))
-  bench('T_G1::add', C2, () => { P = mcl.add(P, P2) })
-  bench('T_G1::dbl', C2, () => { P = mcl.dbl(P) })
-  bench('T_G1::mul', C, () => { P = mcl.mul(P, a) })
-  bench('T_G2::add', C2, () => { Q = mcl.add(Q, Q2) })
-  bench('T_G2::dbl', C2, () => { Q = mcl.dbl(Q) })
-  bench('T_G2::mul', C, () => { Q = mcl.mul(Q, a) })
-  bench('T_hashAndMapToG1', C, () => mcl.hashAndMapToG1(msg))
-  bench('T_hashAndMapToG2', C, () => mcl.hashAndMapToG2(msg))
+  bench('Fr::setByCSPRNG', C, () => a.setByCSPRNG())
+  bench('pairing', C, () => mcl.pairing(P, Q))
+  bench('millerLoop', C, () => mcl.millerLoop(P, Q))
+  bench('finalExp', C, () => mcl.finalExp(e))
+  bench('precomputedMillerLoop', C, () => mcl.precomputedMillerLoop(P, Qcoeff))
+  bench('G1::add', C2, () => { P = mcl.add(P, P2) })
+  bench('G1::dbl', C2, () => { P = mcl.dbl(P) })
+  bench('G1::mul', C, () => { P = mcl.mul(P, a) })
+  bench('G2::add', C2, () => { Q = mcl.add(Q, Q2) })
+  bench('G2::dbl', C2, () => { Q = mcl.dbl(Q) })
+  bench('G2::mul', C, () => { Q = mcl.mul(Q, a) })
+  bench('hashAndMapToG1', C, () => mcl.hashAndMapToG1(msg))
+  bench('hashAndMapToG2', C, () => mcl.hashAndMapToG2(msg))
 
   {
-    let a = new mcl.Fp()
+    const a = new mcl.Fp()
     let b = new mcl.Fp()
     a.setByCSPRNG()
     b.setByCSPRNG()
-    bench('T_Fp::add', C2, () => { b = mcl.add(b, a) })
-    bench('T_Fp::mul', C2, () => { b = mcl.mul(b, a) })
-    bench('T_Fp::sqr', C2, () => { b = mcl.sqr(b) })
-    bench('T_Fp::inv', C2, () => { b = mcl.inv(b) })
+    console.log('Fp')
+    bench('Fp::add', C2, () => { b = mcl.add(b, a) })
+    bench('Fp::mul', C2, () => { b = mcl.mul(b, a) })
+    bench('Fp::sqr', C2, () => { b = mcl.sqr(b) })
+    bench('Fp::inv', C2, () => { b = mcl.inv(b) })
   }
-  let b = new mcl.Fr()
-  b.setByCSPRNG()
-  bench('T_Fr::add', C2, () => { b = mcl.add(b, a) })
-  bench('T_Fr::mul', C2, () => { b = mcl.mul(b, a) })
-  bench('T_Fr::sqr', C2, () => { b = mcl.sqr(b) })
-  bench('T_Fr::inv', C2, () => { b = mcl.inv(b) })
+  {
+    const a = new mcl.Fp2()
+    let b = new mcl.Fp2()
+    a.setInt(3, 4)
+    b.setInt(-3, 9)
+    console.log('Fp2')
+    bench('Fp2::add', C2, () => { b = mcl.add(b, a) })
+    bench('Fp2::mul', C2, () => { b = mcl.mul(b, a) })
+    bench('Fp2::sqr', C2, () => { b = mcl.sqr(b) })
+    bench('Fp2::inv', C2, () => { b = mcl.inv(b) })
+  }
+  {
+    let b = new mcl.Fr()
+    b.setByCSPRNG()
+    console.log('Fr')
+    bench('Fr::add', C2, () => { b = mcl.add(b, a) })
+    bench('Fr::mul', C2, () => { b = mcl.mul(b, a) })
+    bench('Fr::sqr', C2, () => { b = mcl.sqr(b) })
+    bench('Fr::inv', C2, () => { b = mcl.inv(b) })
+  }
 
-  let e2 = mcl.pairing(P, Q)
-  bench('T_GT::add', C2, () => { e2 = mcl.add(e2, e) })
-  bench('T_GT::mul', C2, () => { e2 = mcl.mul(e2, e) })
-  bench('T_GT::sqr', C2, () => { e2 = mcl.sqr(e2) })
-  bench('T_GT::inv', C, () => { e2 = mcl.inv(e2) })
+  {
+    let e2 = mcl.pairing(P, Q)
+    bench('GT::add', C2, () => { e2 = mcl.add(e2, e) })
+    bench('GT::mul', C2, () => { e2 = mcl.mul(e2, e) })
+    bench('GT::sqr', C2, () => { e2 = mcl.sqr(e2) })
+    bench('GT::inv', C, () => { e2 = mcl.inv(e2) })
+  }
 
   Qcoeff.destroy()
 }
