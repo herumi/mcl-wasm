eda1ff4a40d857be3fdec1a669088851b1f67a6f
MITSUNARI Shigeo <herumi@nifty.com>
2021-11-11 13:30:56 +0900

    Workder has not window but self

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const getRandomValues = (buf) => {
     // for Browser
     const crypto = window.crypto || window.msCrypto
     return crypto.getRandomValues(buf)
+  } else if (typeof self === 'object' && typeof self.crypto === 'object' && typeof self.crypto.getRandomValues === 'function') {
+    // for Worker
+    const crypto = self.crypto
+    return crypto.getRandomValues(buf)
   } else {
     // for Node.js
     const crypto = require('crypto')
