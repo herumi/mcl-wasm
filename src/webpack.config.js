c345c6c9b01e68a6471a3db27ebaa3fa9d5e9180
asa-taka <takahiro-asano@cybozu.co.jp>
2021-11-11 22:40:13 +0900

    Apply typescript code for browser

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,14 +1,24 @@
 
 module.exports = {
   mode: 'production',
-  entry: './index.js',
+  entry: './index.ts',
   output: {
     path: __dirname + '/../browser/',
     library: 'mcl',
     libraryTarget: 'umd',
     filename: 'mcl.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
   resolve: {
+    extensions: ['.ts', '.js'],
     fallback: {
       path: false,
       fs: false,
