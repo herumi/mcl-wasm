446100cfed63762977bc7e5acd9cfe5e5400a5db
MITSUNARI Shigeo <herumi@nifty.com>
2024-03-07 13:37:18 +0900

    always use crypto.getRandomValues

--- src/getRandomValues.ts
+++ src/getRandomValues.ts
@@ -1,12 +1,5 @@
 const getRandomValues = (buf: Uint8Array): Uint8Array => {
-  if (typeof self === 'object' && typeof self.crypto === 'object' && typeof self.crypto.getRandomValues === 'function') {
-    // for Browser and Worker
-    return self.crypto.getRandomValues(buf)
-  } else {
-    // for Node.js
-    const crypto = require('crypto')
-    return crypto.randomFillSync(buf)
-  }
+  return crypto.getRandomValues(buf)
 }
 
 export default getRandomValues
