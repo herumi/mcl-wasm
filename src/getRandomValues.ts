22e1c82e7089b4d59706c0d244de1f95efcfdddb
MITSUNARI Shigeo <herumi@nifty.com>
2026-02-08 10:40:33 +0900

    fix error for new TypeScript

--- src/getRandomValues.ts
+++ src/getRandomValues.ts
@@ -1,5 +1,5 @@
 const getRandomValues = (buf: Uint8Array): Uint8Array => {
-  return crypto.getRandomValues(buf)
+  return crypto.getRandomValues(buf as Uint8Array<ArrayBuffer>) as Uint8Array
 }
 
 export default getRandomValues
