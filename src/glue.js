02d2e10730aca86edec70e1d3f487c776dc09668
MITSUNARI Shigeo <herumi@nifty.com>
2026-05-22 12:58:34 +0900

    remove semicolon at the end of line

--- src/glue.js
+++ src/glue.js
@@ -1,28 +1,28 @@
 function createModule(opts) {
-  var wasmBase64 = "@@WASM_BASE64@@";
-  var wasmBytes = Uint8Array.from(atob(wasmBase64), function(c) { return c.charCodeAt(0); });
+  var wasmBase64 = "@@WASM_BASE64@@"
+  var wasmBytes = Uint8Array.from(atob(wasmBase64), function(c) { return c.charCodeAt(0) })
 
-  var memory = new WebAssembly.Memory({ initial: 32 });
+  var memory = new WebAssembly.Memory({ initial: 32 })
 
   var imports = {
     env: {
       memory: memory,
       cryptoGetRandomValues: function(ptr, size) {
-        var buf = new Uint8Array(memory.buffer);
-        var a = new Uint8Array(size);
-        opts.cryptoGetRandomValues(a);
-        buf.set(a, ptr);
+        var buf = new Uint8Array(memory.buffer)
+        var a = new Uint8Array(size)
+        opts.cryptoGetRandomValues(a)
+        buf.set(a, ptr)
       }
     }
-  };
+  }
 
   return WebAssembly.instantiate(wasmBytes, imports).then(function(result) {
-    var instance = result.instance;
-    var mod = {};
+    var instance = result.instance
+    var mod = {}
 
-    mod.wasmMemory = memory;
-    mod.HEAP8 = new Int8Array(memory.buffer);
-    mod.HEAP32 = new Int32Array(memory.buffer);
+    mod.wasmMemory = memory
+    mod.HEAP8 = new Int8Array(memory.buffer)
+    mod.HEAP32 = new Int32Array(memory.buffer)
 
     // Export mclBn* wasm functions with _ prefix
     var exports = instance.exports
@@ -35,15 +35,15 @@ function createModule(opts) {
     mod['_free'] = exports.free
 
     // Stack pointer operations (direct wasm exports)
-    mod.stackSave = exports.stackSave;
-    mod.stackAlloc = exports.stackAlloc;
-    mod.stackRestore = exports.stackRestore;
+    mod.stackSave = exports.stackSave
+    mod.stackAlloc = exports.stackAlloc
+    mod.stackRestore = exports.stackRestore
 
     // Call global constructors if present
-    if (exports.__wasm_call_ctors) exports.__wasm_call_ctors();
+    if (exports.__wasm_call_ctors) exports.__wasm_call_ctors()
 
-    return mod;
-  });
+    return mod
+  })
 }
 
-if (typeof module !== 'undefined') module.exports = createModule;
+if (typeof module !== 'undefined') module.exports = createModule
